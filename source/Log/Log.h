b43bde89629546623adca7d1b0de9370652ab5fc
Cezary Androsiuk PC <czandros@wp.pl>
2025-07-02 16:01:34 +0200

    v1.10.1 - added flag for spliting debug and trace logs (SPLIT_DEBUG_AND_TRACE_LOGS)

--- source/Log/Log.h
+++ source/Log/Log.h
@@ -61,7 +61,7 @@ DOLTV_F(...)    - MyClass(int c, int b, int a){DOLTV_F("1, 2, 3") or DOLTV_F("1,
 #define USE_QT_SUPPORT false /// in external layer of abstraction QString will be used insead of std::string
 /// it allows to use I(someQStringVariable); instead of  I(someQStringVariable.toStdString());
 
-#define SPLIT_DEBUG_AND_TRACE_LOGS true /// if true, then debug and trace logs will be saved in different files
+#define SPLIT_DEBUG_AND_TRACE_LOGS false /// if true, then debug and trace logs will be saved in different files
 #define ENABLE_MANAGING_LOG_INSTANCE_LIFE_TIME false /// decides if 'SingletonManager' class is a friend and could manage singleton life time by Log::instance
 #define EST_FUNCTION_LENGTH 70 /// estimated function name length what will be reserved while creating log
 #define SHORTER_FUNCTION_FILL_CHARACTER ' ' /// characters that fills area before function name to fit estimated function name length
@@ -91,9 +91,12 @@ DOLTV_F(...)    - MyClass(int c, int b, int a){DOLTV_F("1, 2, 3") or DOLTV_F("1,
 // #include "LogSession.h"
 
 extern const char *version;
-// #if SPLIT_DEBUG_AND_TRACE_LOGS
+#if SPLIT_DEBUG_AND_TRACE_LOGS
 extern const char *debugLogsOutputDirectory;
 extern const char *traceLogsOutputDirectory;
+#else
+extern const char *logsOutputDirectory;
+#endif /// SPLIT_DEBUG_AND_TRACE_LOGS
 extern const char *logsInfoFileName;
 
 typedef std::string str; /// string
@@ -280,8 +283,12 @@ private:
 
     std::string m_startTime;
 
+#if SPLIT_DEBUG_AND_TRACE_LOGS
     std::ofstream m_debugLogFile;
     std::ofstream m_traceLogFile;
+#else
+    std::ofstream m_logFile;
+#endif /// SPLIT_DEBUG_AND_TRACE_LOGS
 
     std::unordered_map<std::string, int> m_filesPaths;
 
